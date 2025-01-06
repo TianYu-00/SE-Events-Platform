95b074ae975e70b9c1daf77aa795db88c4962c53
TianYu-00 <imniyeye@gmail.com>
2025-01-06 20:25:29 +0000

    test: add additional tests for event fetching endpoint

--- backend/__tests__/events/api_events.test.js
+++ backend/__tests__/events/api_events.test.js
@@ -11,4 +11,32 @@ describe("GET /api/events", () => {
   test("should return a 200 status code", async () => {
     await request(app).get("/api/events").expect(200);
   });
+
+  test("should return success = true", async () => {
+    const { body } = await request(app).get("/api/events");
+    expect(body.success).toBe(true);
+  });
+
+  test("should return correct event object", async () => {
+    const { body } = await request(app).get("/api/events");
+    for (const user of body.data) {
+      expect(user).toMatchObject({
+        event_name: expect.any(String),
+        event_start_date: expect.any(String),
+        event_end_date: expect.any(String),
+        event_full_address: expect.any(String),
+        event_description: expect.any(String),
+        event_organizer: expect.any(Number),
+        event_capacity: expect.any(Number),
+        event_attendees: expect.any(Number),
+        event_cost_in_pence: expect.any(Number),
+        event_contact_email: expect.any(String),
+        event_contact_phone_prefix: expect.any(String),
+        event_contact_phone: expect.any(String),
+        event_website: expect.any(String),
+        event_tags: expect.any(Array),
+        event_thumbnail: expect.any(String),
+      });
+    }
+  });
 });
