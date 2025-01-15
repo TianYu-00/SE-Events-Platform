0fed2a59927b86afa7e7921a4c41cba477254246
TianYu-00 <imniyeye@gmail.com>
2025-01-15 04:14:18 +0000

    feat: add updateEvent function to handle event updates

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -76,3 +76,29 @@ export const deleteEvents = async ({ listOfEventIds }) => {
     throw error;
   }
 };
+
+export const updateEvent = async (eventId, eventData) => {
+  const data = {
+    event_name: eventData.eventName,
+    event_start_date: eventData.eventStartDate,
+    event_end_date: eventData.eventEndDate,
+    event_full_address: eventData.eventAddress,
+    event_description: eventData.eventDescription,
+    event_organizer_id: eventData.eventOrganizerUserId,
+    event_capacity: eventData.eventCapacity,
+    event_attendees: eventData.eventAttendees,
+    event_cost_in_pence: eventData.eventCostInPence,
+    event_contact_email: eventData.eventContactEmail,
+    event_contact_phone_prefix: eventData.eventContactPhonePrefix,
+    event_contact_phone: eventData.eventContactPhone,
+    event_website: eventData.eventWebsite,
+    event_tags: eventData.eventTags,
+    event_thumbnail: eventData.eventThumbnail,
+  };
+  try {
+    const response = await api.patch(`/events/${eventId}`, data);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
