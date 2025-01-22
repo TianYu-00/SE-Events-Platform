18b72a7ae1ebec42aab6c2e046bbdbe50fff5e6c
TianYu-00 <imniyeye@gmail.com>
2025-01-22 18:58:08 +0000

    feat: enhance event details page with improved layout and purchase functionality

--- frontend/src/pages/Landing_EventDetails.jsx
+++ frontend/src/pages/Landing_EventDetails.jsx
@@ -1,12 +1,24 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { getEvent } from "../api";
 import { moneyFormatter } from "../components/MoneyFormatter";
 import { dateFormatter } from "../components/DateFormatter";
+import {
+  TbCalendar,
+  TbCalendarClock,
+  TbCash,
+  TbMail,
+  TbMapPin,
+  TbPhone,
+  TbTags,
+  TbUsers,
+  TbWorld,
+} from "react-icons/tb";
 
 function Landing_EventDetails() {
   const [event, setEvent] = useState(null);
   const { eventId } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const runFetchEvent = async () => {
@@ -19,30 +31,163 @@ function Landing_EventDetails() {
     runFetchEvent();
   }, []);
 
+  const handle_EventPurchase = async () => {
+    if (event.event_cost_in_pence > 0) {
+      navigate(`/payment?event_id=${eventId}`);
+    } else {
+      console.log("ITS FREE!");
+    }
+  };
+
+  if (!event) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div className="text-copy-primary">
-      <img src={event.event_thumbnail} alt="" />
-      <h2>{event.event_name}</h2>
-      <p>{event.event_description}</p>
-      <p>{event.event_attendees}</p>
-      <p>{event.event_capacity}</p>
-      <p>
-        {event.event_contact_phone_prefix} {event.event_contact_phone}
-      </p>
-      <p>{event.event_contact_email}</p>
-      <p>£{moneyFormatter(event.event_cost_in_pence)}</p>
-      <p>{dateFormatter(event.event_created_at, 3)}</p>
-      <p>{dateFormatter(event.event_modified_at, 3)}</p>
-      <p>{dateFormatter(event.event_start_date, 3)}</p>
-      <p>{dateFormatter(event.event_end_date, 3)}</p>
-      <p>{event.event_full_address}</p>
-      <p>{event.event_tags}</p>
-      <p>{event.event_website}</p>
+    <div className="text-copy-primary min-h-[calc(100vh-5rem)] max-w-screen-xl mx-auto p-4">
+      <div className="shadow-md rounded-lg">
+        <img src={event.event_thumbnail} alt={`${event.event_name} image`} className="rounded-lg w-full" />
+      </div>
+
+      <div className="mt-4 bg-card border border-border rounded-md p-4 w-full shadow-md">
+        <h2 className="text-2xl md:text-3xl font-bold mb-2">{event.event_name}</h2>
+        <p className="text-copy-secondary">{event.event_description}</p>
+      </div>
+
+      <div className="my-4 grid grid-cols-1 md:grid-cols-3 gap-4">
+        <div className="md:col-span-2">
+          <div className="bg-card border border-border rounded-md p-4 grid grid-cols-1 md:grid-cols-2 gap-4 shadow-md">
+            {/* Event Details */}
+            <div className="col-span-1 md:col-span-2">
+              <h2 className="text-xl md:text-2xl font-bold mb-2">Event Details</h2>
+            </div>
+
+            {/* Start Date */}
+            {eventDetailsHelper({
+              icon: <TbCalendarClock size={23} />,
+              title: "Start Date",
+              data: dateFormatter(event.event_start_date, 3),
+            })}
+
+            {/* End Date */}
+            {eventDetailsHelper({
+              icon: <TbCalendarClock size={23} />,
+              title: "End Date",
+              data: dateFormatter(event.event_end_date, 3),
+            })}
+
+            {/* Location */}
+            {eventDetailsHelper({
+              icon: <TbMapPin size={23} />,
+              title: "Location",
+              data: event.event_full_address,
+            })}
+
+            {/* Attendees */}
+            {eventDetailsHelper({
+              icon: <TbUsers size={23} />,
+              title: "Attendees",
+              data: `${event.event_attendees} / ${event.event_capacity}`,
+            })}
+
+            {/* Phone */}
+            {eventDetailsHelper({
+              icon: <TbPhone size={23} />,
+              title: "Phone",
+              data: `${event.event_contact_phone_prefix} ${event.event_contact_phone}`,
+            })}
+
+            {/* Email */}
+            {eventDetailsHelper({
+              icon: <TbMail size={23} />,
+              title: "Email",
+              data: event.event_contact_email,
+            })}
+
+            {/* Website */}
+            {eventDetailsHelper({
+              icon: <TbWorld size={23} />,
+              title: "Website",
+              data: event.event_website,
+            })}
+
+            {/* Created At */}
+            {eventDetailsHelper({
+              icon: <TbCalendar size={23} />,
+              title: "Created At",
+              data: dateFormatter(event.event_created_at, 3),
+            })}
+
+            {/* Modified At */}
+            {eventDetailsHelper({
+              icon: <TbCalendar size={23} />,
+              title: "Modified At",
+              data: dateFormatter(event.event_modified_at, 3),
+            })}
+
+            {/* Price */}
+            {eventDetailsHelper({
+              icon: <TbCash size={23} />,
+              title: "Cost",
+              data: `£${moneyFormatter(event.event_cost_in_pence)}`,
+            })}
+
+            {/* Tags */}
+            <div className="flex flex-row space-x-3 items-start col-span-1 md:col-span-2">
+              <div className="rounded-full p-2 bg-background w-fit h-fit">
+                <TbTags size={23} />
+              </div>
+              <div>
+                <p className="font-semibold">Tags</p>
+                <div className="text-copy-secondary flex flex-wrap gap-2">{tagDisplayHelper(event.event_tags)}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="">
+          <div className="bg-card border border-border rounded-md p-4 flex flex-col justify-center items-center w-full h-full shadow-md">
+            <div className="flex flex-col items-center">
+              <h2 className="text-2xl md:text-5xl font-bold mb-2">£{moneyFormatter(event.event_cost_in_pence)}</h2>
+              <p className="text-copy-secondary">per ticket</p>
+              <button
+                className="p-3 bg-cta hover:bg-cta-active text-cta-text rounded-md font-semibold mt-5"
+                onClick={handle_EventPurchase}
+              >
+                {event.event_cost_in_pence > 0 ? "Purchase Now" : "Signup Now"}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default Landing_EventDetails;
+
+function eventDetailsHelper({ icon, title, data }) {
+  return (
+    <div className="flex flex-row space-x-3 items-center">
+      <div className="rounded-full p-2 bg-background w-fit h-fit">{icon}</div>
+
+      <div className="">
+        <p className="font-semibold">{title}</p>
+        <p className="text-copy-secondary">{data}</p>
+      </div>
+    </div>
+  );
+}
+
+function tagDisplayHelper(tags) {
+  return tags.map((tag, index) => {
+    return (
+      <span key={index} className="p-1 px-2 bg-background rounded-md">
+        {tag}
+      </span>
+    );
+  });
+}
 
 /*
 
