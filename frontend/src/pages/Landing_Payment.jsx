8ffe2cafdaeb7940421b8fcbcc25b2a35724d401
TianYu-00 <imniyeye@gmail.com>
2025-01-27 14:34:17 +0000

    feat: implement error handling with toast notifications in Landing_Payment component

--- frontend/src/pages/Landing_Payment.jsx
+++ frontend/src/pages/Landing_Payment.jsx
@@ -6,14 +6,17 @@ import { createPayment, getEvent } from "../api";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useTheme } from "../context/ThemeContext";
 import { useUser } from "@clerk/clerk-react";
-import Lottie from "lottie-react";
-import failAnimation from "../assets/lotties/Animation_Fail - 1737087453027.json";
+
+import { toast } from "react-toastify";
+import useErrorChecker from "../hooks/useErrorChecker";
 
 // Stripe
 const STRIPE_PUBLISHABLE_KEY = import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY;
 const stripePromise = loadStripe(STRIPE_PUBLISHABLE_KEY);
 
 function Landing_Payment() {
+  const checkError = useErrorChecker();
+
   const navigate = useNavigate();
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
@@ -26,7 +29,6 @@ function Landing_Payment() {
 
   const [event, setEvent] = useState(null);
   const [eventPrice, setEventPrice] = useState(null);
-  const [isSoldOut, setIsSoldOut] = useState(false);
 
   const { user } = useUser();
 
@@ -44,11 +46,9 @@ function Landing_Payment() {
       } catch (error) {
         const errorCode = error.response?.data?.code;
         const clientErrorCode = error.code;
-
+        checkError(error);
         if (errorCode === "EVENT_NOT_FOUND" || errorCode === "INVALID_PARAMS" || clientErrorCode === "OUTDATED_EVENT") {
           navigate(`/`);
-        } else {
-          console.error(error);
         }
       }
     };
@@ -64,10 +64,10 @@ function Landing_Payment() {
         setClientSecret(response.data.clientSecret);
         setEventPrice(response.data.price);
       } catch (error) {
-        console.error(error);
+        checkError(error);
         const errorCode = error.response.data.code;
         if (errorCode === "NO_TICKETS_AVAILABLE") {
-          setIsSoldOut(true);
+          navigate(`/`);
         }
       }
     };
@@ -76,24 +76,6 @@ function Landing_Payment() {
       runCreatePaymentIntent();
     }
   }, [event, user]);
-
-  if (isSoldOut) {
-    return (
-      <div className="text-copy-primary w-full min-h-[calc(100vh-5rem)] flex justify-center items-center flex-col">
-        <div className="w-40">
-          <Lottie animationData={failAnimation} loop autoplay />
-        </div>
-        <h2 className="font-bold text-xl">Oops! Looks like tickets for this event are all sold out!</h2>
-
-        <button
-          className="p-2 bg-cta text-cta-text rounded-md hover:bg-cta-active mt-6"
-          onClick={() => navigate("/events", { replace: true })}
-        >
-          Return To Events
-        </button>
-      </div>
-    );
-  }
 
   return (
     <div className="w-full min-h-[calc(100vh-5rem)] flex justify-center items-center">
