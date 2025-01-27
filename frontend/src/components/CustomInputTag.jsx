de26f896d3cf2b1c11e365d6545eabd95da0490c
TianYu-00 <imniyeye@gmail.com>
2025-01-27 04:41:48 +0000

    feat: add drag-and-drop functionality for tag reordering in CustomInputTag component + fix tags going off screen

--- frontend/src/components/CustomInputTag.jsx
+++ frontend/src/components/CustomInputTag.jsx
@@ -3,6 +3,7 @@ import { TbX } from "react-icons/tb";
 
 function CustomInputTag({ tags = [], setTags }) {
   const [input, setInput] = useState("");
+  const [draggedEventTagIndex, setDraggedEventTagIndex] = useState(null);
 
   const handle_KeyDownSubmitTag = (e) => {
     if (e.key === "Enter" || e.key === ",") {
@@ -20,14 +21,42 @@ function CustomInputTag({ tags = [], setTags }) {
     setTags(newTags);
   };
 
+  const handle_DragStart = (index) => {
+    setDraggedEventTagIndex(index);
+  };
+
+  const handle_TagDrop = (dropIndex) => {
+    if (draggedEventTagIndex === dropIndex) return;
+
+    const updatedTags = [...tags];
+    const [draggedTag] = updatedTags.splice(draggedEventTagIndex, 1);
+    updatedTags.splice(dropIndex, 0, draggedTag);
+
+    setTags(updatedTags);
+    setDraggedEventTagIndex(null);
+  };
+
+  const handle_TagDragOver = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div>
       {tags.length > 0 && (
-        <div className="flex space-x-4 mb-2">
+        <div className="flex flex-wrap gap-2">
           {tags.map((tag, index) => (
-            <div key={index} className="p-2 bg-card text-copy-primary border border-border rounded-md flex space-x-2">
+            <div
+              key={index}
+              className={`p-2 bg-card text-copy-primary border border-border rounded-md flex space-x-2 hover:cursor-pointer ${
+                draggedEventTagIndex === index ? "bg-blue-500/30" : ""
+              }`}
+              draggable
+              onDragStart={() => handle_DragStart(index)}
+              onDrop={() => handle_TagDrop(index)}
+              onDragOver={handle_TagDragOver}
+            >
               <span>{tag}</span>
-              <button className="text-red-500 px-1" onClick={() => handle_RemoveTag(tag)}>
+              <button className="text-red-500 px-1 hover:text-red-400" onClick={() => handle_RemoveTag(tag)}>
                 <TbX size={17} strokeWidth={3} />
               </button>
             </div>
@@ -42,6 +71,7 @@ function CustomInputTag({ tags = [], setTags }) {
         onKeyDown={handle_KeyDownSubmitTag}
         placeholder="Press enter or comma to add tag"
         className="block w-full rounded-md shadow-sm p-2 border border-border bg-card text-copy-primary focus:outline-none focus:border-border col-span-2"
+        maxLength="28"
       />
     </div>
   );
