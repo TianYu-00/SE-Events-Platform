b95c043c8600cad8daacff23aafcc4110c918e0f
TianYu-00 <imniyeye@gmail.com>
2025-01-27 23:54:28 +0000

    fix: delete event test to include tokens when making calls

--- backend/__tests__/events/api_events_deleteEvents.test.js
+++ backend/__tests__/events/api_events_deleteEvents.test.js
@@ -1,4 +1,4 @@
-const { app, request, db, seed, data } = require("../../utils/test-utils/index");
+const { app, request, db, seed, data, adminToken, userToken } = require("../../utils/test-utils/index");
 
 afterAll(() => {
   return db.end();
@@ -14,28 +14,40 @@ describe("DELETE /api/events", () => {
   const emptyEventIds = [];
 
   test("should return 200 and success message when valid event IDs are provided", async () => {
-    const { body } = await request(app).delete("/api/events").send({ event_id: validEventIds }).expect(200);
+    const { body } = await request(app)
+      .delete("/api/events")
+      .auth(adminToken, { type: "bearer" })
+      .send({ event_id: validEventIds })
+      .expect(200);
     expect(body.success).toBe(true);
     expect(body.data.deletedIds).toEqual(expect.arrayContaining(validEventIds));
     expect(body.data.failedToDeleteIds.length).toBe(0);
   });
 
   test("should return 400 and error code when eventIds array is empty", async () => {
-    const { body } = await request(app).delete("/api/events").send({ event_id: emptyEventIds }).expect(400);
+    const { body } = await request(app)
+      .delete("/api/events")
+      .auth(adminToken, { type: "bearer" })
+      .send({ event_id: emptyEventIds })
+      .expect(400);
 
     expect(body.success).toBe(false);
     expect(body.code).toBe("BODY_CONTENT_INCOMPLETE");
   });
 
   test("should return 400 and error code when eventIds are missing", async () => {
-    const { body } = await request(app).delete("/api/events").send({}).expect(400);
+    const { body } = await request(app).delete("/api/events").auth(adminToken, { type: "bearer" }).send({}).expect(400);
 
     expect(body.success).toBe(false);
     expect(body.code).toBe("BODY_CONTENT_INCOMPLETE");
   });
 
   test("should return 400 and error code when no events are deleted", async () => {
-    const { body } = await request(app).delete("/api/events").send({ event_id: invalidEventIds }).expect(400);
+    const { body } = await request(app)
+      .delete("/api/events")
+      .auth(adminToken, { type: "bearer" })
+      .send({ event_id: invalidEventIds })
+      .expect(400);
 
     expect(body.success).toBe(false);
     expect(body.code).toBe("NO_EVENT_DELETED");
@@ -44,11 +56,22 @@ describe("DELETE /api/events", () => {
   test("should return 200 with a partial deletion when some events exist and others do not", async () => {
     const { body } = await request(app)
       .delete("/api/events")
+      .auth(adminToken, { type: "bearer" })
       .send({ event_id: [...validEventIds, ...invalidEventIds] })
       .expect(200);
 
     expect(body.success).toBe(true);
     expect(body.data.deletedIds).toEqual(expect.arrayContaining(validEventIds));
     expect(body.data.failedToDeleteIds).toEqual(expect.arrayContaining(invalidEventIds));
+  });
+
+  test("user should not have access to this route", async () => {
+    const { body } = await request(app)
+      .delete("/api/events")
+      .auth(userToken, { type: "bearer" })
+      .send({ event_id: [...validEventIds, ...invalidEventIds] })
+      .expect(403);
+
+    expect(body.code).toBe("ACCESS_DENIED");
   });
 });
--- backend/utils/test-utils/index.js
+++ backend/utils/test-utils/index.js
@@ -3,3 +3,5 @@ exports.db = require("../../db/connection");
 exports.request = require("supertest");
 exports.seed = require("../../db/seed");
 exports.data = require("../../db/test_data");
+exports.userToken = process.env.CLERK_TESTING_USER_TOKEN;
+exports.adminToken = process.env.CLERK_TESTING_ADMIN_TOKEN;
