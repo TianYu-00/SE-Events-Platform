06b1f4dc1b523bc0bf0d40b4519586544ff6ea26
TianYu-00 <imniyeye@gmail.com>
2025-01-28 01:38:55 +0000

    fix: update CORS configuration to use environment variable for frontend URL

--- backend/listen.js
+++ backend/listen.js
@@ -3,5 +3,5 @@ const PORT = process.env.PORT || 9090;
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}...`);
-  console.log(`Local: http://127.0.0.1:${PORT}`);
+  // console.log(`Local: http://127.0.0.1:${PORT}`);
 });
