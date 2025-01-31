a2c28a7d34e6232373467be4ece182c979f56a1f
TianYu-00 <imniyeye@gmail.com>
2025-01-31 01:17:56 +0000

    feat: improve table search + table pagination accessibility

--- frontend/src/components/TablePagination.jsx
+++ frontend/src/components/TablePagination.jsx
@@ -12,6 +12,7 @@ const Pagination = ({ table }) => {
           onChange={(e) => {
             table.setPageSize(Number(e.target.value));
           }}
+          aria-label="select events per page"
         >
           {[10, 20, 30, 40, 50].map((pageSize) => (
             <option key={pageSize} value={pageSize}>
@@ -23,16 +24,18 @@ const Pagination = ({ table }) => {
 
       <div className="flex items-center space-x-2">
         <button
-          className="p-2 rounded-md bg-cta text-cta-text hover:bg-cta-active disabled:opacity-50"
+          className="p-2 rounded-md bg-cta text-cta-text hover:bg-cta-active disabled:opacity-50 disabled:cursor-not-allowed"
           onClick={() => table.setPageIndex(0)}
           disabled={!table.getCanPreviousPage()}
+          aria-label="navigate to first page"
         >
           <TbCaretLeftFilled size={17} />
         </button>
         <button
-          className="p-2 rounded-md bg-cta text-cta-text hover:bg-cta-active disabled:opacity-50 px-4"
+          className="p-2 rounded-md bg-cta text-cta-text hover:bg-cta-active disabled:opacity-50 px-4 disabled:cursor-not-allowed"
           onClick={() => table.previousPage()}
           disabled={!table.getCanPreviousPage()}
+          aria-label="previous page"
         >
           {/* <TbCaretLeft size={17} /> */}
           Previous
@@ -48,21 +51,24 @@ const Pagination = ({ table }) => {
               table.setPageIndex(page);
             }}
             className="w-16 p-2 rounded-md border border-border text-center bg-card text-copy-primary"
+            aria-label="enter page number to navigate to"
           />
           <span className="ml-1 text-copy-primary/70">of {table.getPageCount()}</span>
         </span>
         <button
-          className="p-2 rounded-md bg-cta text-cta-text hover:bg-cta-active disabled:opacity-50 px-4"
+          className="p-2 rounded-md bg-cta text-cta-text hover:bg-cta-active disabled:opacity-50 px-4 disabled:cursor-not-allowed"
           onClick={() => table.nextPage()}
           disabled={!table.getCanNextPage()}
+          aria-label="next page"
         >
           {/* <TbCaretRight size={17} /> */}
           Next
         </button>
         <button
-          className="p-2 rounded-md bg-cta text-cta-text hover:bg-cta-active disabled:opacity-50"
+          className="p-2 rounded-md bg-cta text-cta-text hover:bg-cta-active disabled:opacity-50 disabled:cursor-not-allowed"
           onClick={() => table.setPageIndex(table.getPageCount() - 1)}
           disabled={!table.getCanNextPage()}
+          aria-label="navigate to last page"
         >
           <TbCaretRightFilled size={17} />
         </button>
